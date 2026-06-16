f02f5fbb3e38c3edca6acf10dcd50c8afceb49e4
PC <PC@DESKTOP-MT20223>
2026-06-16 21:59:15 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoLClientTool.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
